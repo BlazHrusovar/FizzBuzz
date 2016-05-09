bc9dccc8d90de4c3dc7dccff5bf3f4b68abca805
BlazHrusovar <b.hrusovar@gmail.com>
2016-05-09 16:59:06 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        Scanner reader = new Scanner(System.in); 
+        System.out.println("Vnesi stevilo: ");
+        int n = reader.nextInt(); 
+        
+        for (int i = 1; i <= n; i++){
+            if (i%3 == 0){
+                System.out.println("fizz");
+            }
+            else if(i%5 == 0){
+                System.out.println("buzzzz");
+            }
+            else if(i%3 == 0 && i%5 == 0){
+                System.out.println("fizzbuzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
